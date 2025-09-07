c0b9dc189c09ce183bc24830808122a55a591ee4
MinhoSantiago <romolosantiagomi@gmail.com>
2025-09-07 20:23:22 -0300

    Aula de Arrays Multidimensionais e arrumando a base do código para receber aulas de POO

---
+++ src/academia/devdojo/maratonajava/introducao/Aula06ArraysMultidimesionais01.java
@@ -0,0 +1,40 @@
+package academia.devdojo.maratonajava.introducao;
+
+public class Aula06ArraysMultidimesionais01 {
+    public static void main(String[] args) {
+        int [][] dias = new int[3][3];
+
+        dias[0][0]=1;
+        dias[0][1]=2;
+        dias[0][2]=3;
+
+        dias[1][0]=4;
+        dias[1][1]=5;
+        dias[1][2]=6;
+
+        dias[2][0]=7;
+        dias[2][1]=8;
+        dias[2][2]=9;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+
+
+            System.out.println("");
+            System.out.println("-----------VERSÃO FOREACH-------------");
+            System.out.println("");
+
+
+
+            for(int[]arrbase:dias){
+                for(int num: arrbase){
+                    System.out.println(num);
+                }
+            }
+
+
+        }
+    }
+}
