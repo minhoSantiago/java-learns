8cb1edea1d4760a0347e348dbf8166456f80c622
MinhoSantiago <romolosantiagomi@gmail.com>
2025-09-07 21:26:29 -0300

    Corrigi erros de sintaxe que quando rodava o código inteiro sem comentar os exercicios anteriores quebrava o código e também tentei melhorar os loops nos arrays

---
+++ src/academia/devdojo/maratonajava/introducaoClasses/domain/Personagem.java
@@ -0,0 +1,8 @@
+package academia.devdojo.maratonajava.introducaoClasses;
+
+public class Personagem {
+    public String nome;
+    public int idade;
+    public String animacao;
+
+}
---
+++ src/academia/devdojo/maratonajava/introducaoClasses/teste/personagemTeste1.java
@@ -0,0 +1,15 @@
+package academia.devdojo.maratonajava.introducaoClasses.teste;
+
+import academia.devdojo.maratonajava.introducaoClasses.domain.Personagem;
+
+public class pessoaTeste01 {
+    public static void main(String[] args) {
+        Personagem giomey = new Personagem();
+            giomey.nome = "Gyomei Himejima";
+            giomey.idade = 25;
+            giomey.animacao = "Demon Slayer";
+        System.out.println(giomey.nome);
+        System.out.println(giomey.idade);
+        System.out.println(giomey.animacao);
+    }
+}
